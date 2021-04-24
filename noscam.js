128c1b9ff35cd7c064b9a8c28c154179c4c7e3c3
AshleyBilbrey <44065883+AshleyBilbrey@users.noreply.github.com>
2021-04-23 21:27:49 -0700

    Add index, report, and partial pages

--- noscam.js
+++ noscam.js
@@ -1,2 +1,25 @@
 //NoScam, a project by Ashley Bilbrey
 
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+    res.render('index', {
+        callcount: 5,
+        pagename: "Home"
+    });
+});
+
+app.get('/report', function(req, res) {
+    res.render('report', {
+        pagename: "Report"
+    })
+})
+
+app.listen(port, () => {
+  console.log(`NoScam listening at http://localhost:${port}`)
+})
